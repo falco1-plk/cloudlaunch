07483348c80f4662ead93c725a6d3dc85420d63f
falco1-plk <aishwaryjaiswal6@gamil.com>
2026-03-26 14:30:55 +0530

    Final demo update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-   res.end(`
+  res.end(`
   <h1>CloudLaunch FINAL DEMO 🚀</h1>
-  <p>Auto deployment working successfully</p>
+  <p>Auto deployment working successfully!</p>
 `);
 });
 
